0bca9b491cdb2e4f3037d9e06889d823444c46b7
chriswebdv <chriswebdv@gmail.com>
2024-10-15 18:32:56 -0600

    Update part-2.py

--- part-2.py
+++ part-2.py
@@ -23,3 +23,14 @@ print(f"Integer part:{int(num)}")
 print(f"Decimal part: {num - int(num)}")
 
 #write and function with check the age of the user
+
+# Write your solution here
+
+age = int(input("What is your age? "))
+
+if age < 5 and age >= 0:
+    print("I suspect you can't write quite yet...")
+elif age < 0:
+    print("That must be a mistake")
+else:
+    print(f"Ok, you're {age} years old")
