07bcaee479c76f6d735801e9be4ecfcdb3e3df94
chriswebdv <chriswebdv@gmail.com>
2024-10-24 17:58:47 -0600

    write code

--- part-3.py
+++ part-3.py
@@ -5,4 +5,12 @@ while num >= 2 and num <= 30:
     if num % 2 == 0:
         print(num)
         num += 2
+        
 #number limit using a while loop
+# Write your solution here
+num = int(input("Upper limit: "))
+i = 1
+while num > 0 and i < num:
+    print(i)
+    i += 1
+    
